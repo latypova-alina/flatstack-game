d7c1eb61d31474b2dc28e4e9ec2bdfdbfd06f4ea
SergeyBasharkin <rock-n-rolllll@bk.ru>
2016-08-17 11:30:53 +0300

    incorrect relations are removed

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -4,13 +4,11 @@ FactoryGirl.define do
     player_1 1
     player_2 nil
     state "search"
-    factory :game_with_3_rounds do
-      before(:create) do
-        create(:user, id: 1)
-        create(:round, game_id: 1)
-        create(:round, game_id: 1)
-        create(:round, game_id: 1)
-      end
+    before(:create) do
+      create(:user, id: 1)
+      create(:round, game_id: 1)
+      create(:round, game_id: 1)
+      create(:round, game_id: 1)
     end
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Game, type: :model do
-  let!(:game) { create :game_with_3_rounds }
+  let!(:game) { create :game }
   it { is_expected.to validate_presence_of :player_1 }
   it { should validate_inclusion_of(:state).in_array(%w(search started finished)) }
   it "valid" do
