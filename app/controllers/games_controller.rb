994a847ab35f75c956d88d950c00f63534ed314f
exosty <jarisob@gmail.com>
2016-08-18 10:19:35 +0300

    game creating without logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,18 @@
 class GamesController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+    @game = Game.new
+    if @game.save
+      flash[:notice] = "Game successfully created."
+    else
+      flash[:notice] = "Error creating game."
+    end
+    
+    redirect_to root_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
 
   root to: "games#index"
+  post "/games" => "games#create"
 end
